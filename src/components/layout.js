332952199fac128e0a1a68059bde70bfa086264a
Carlos Benetti <benetticarlos1@gmail.com>
2021-09-09 20:05:57 -0300

    delete useless files

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,22 +2,16 @@ import React from 'react'
 
 import './variables.css'
 import './global.css'
-import Seo from './seo'
-import Navigation from './navigation'
-import Footer from './footer'
-class Template extends React.Component {
+class Layout extends React.Component {
   render() {
     const { children } = this.props
 
     return (
       <>
-        <Seo />
-        <Navigation />
         <main>{children}</main>
-        <Footer />
       </>
     )
   }
 }
 
-export default Template
+export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,18 @@
 import React from 'react'
 import { graphql } from 'gatsby'
-import get from 'lodash/get'
 
 import Layout from '../components/layout'
-import Hero from '../components/hero'
-import ArticlePreview from '../components/article-preview'
 
 class RootIndex extends React.Component {
   render() {
-    const posts = get(this, 'props.data.allContentfulBlogPost.nodes')
-    const [author] = get(this, 'props.data.allContentfulPerson.nodes')
+    // const posts = get(this, 'props.data.allContentfulBlogPost.nodes')
+    // const [author] = get(this, 'props.data.allContentfulPerson.nodes')
 
     return (
-      <Layout location={this.props.location}>
-        <Hero
-          image={author.heroImage.gatsbyImageData}
-          title={author.name}
-          content={author.shortBio.shortBio}
-        />
-        <ArticlePreview posts={posts} />
+      <Layout>
+        <div>
+          HOME
+        </div>
       </Layout>
     )
   }
