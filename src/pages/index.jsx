cc8fc0809deebbae4ebe2abdf00c03600877978d
Carlos Benetti <benetticarlos1@gmail.com>
2021-10-06 14:59:19 -0300

    graphQL err solved

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
-import { graphql } from 'gatsby'
+// import { graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
 
 class RootIndex extends React.Component {
   render() {
-    // const posts = get(this, 'props.data.allContentfulBlogPost.nodes')
-    // const [author] = get(this, 'props.data.allContentfulPerson.nodes')
-
     return (
       <Layout>
         <div>HOME</div>
@@ -17,47 +14,3 @@ class RootIndex extends React.Component {
 }
 
 export default RootIndex
-
-export const pageQuery = graphql`
-  query HomeQuery {
-    allContentfulBlogPost(sort: { fields: [publishDate], order: DESC }) {
-      nodes {
-        title
-        slug
-        publishDate(formatString: "MMMM Do, YYYY")
-        tags
-        heroImage {
-          gatsbyImageData(
-            layout: FULL_WIDTH
-            placeholder: BLURRED
-            width: 424
-            height: 212
-          )
-        }
-        description {
-          childMarkdownRemark {
-            html
-          }
-        }
-      }
-    }
-    allContentfulPerson(
-      filter: { contentful_id: { eq: "15jwOBqpxqSAOy2eOO4S0m" } }
-    ) {
-      nodes {
-        name
-        shortBio {
-          shortBio
-        }
-        title
-        heroImage: image {
-          gatsbyImageData(
-            layout: CONSTRAINED
-            placeholder: BLURRED
-            width: 1180
-          )
-        }
-      }
-    }
-  }
-`
