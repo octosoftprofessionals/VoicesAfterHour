9667c0146b2db5d9e3eb7fe5ea1bccd212c43c82
Carlos Benetti <benetticarlos1@gmail.com>
2021-10-06 16:51:40 -0300

    color pallete

---
+++ src/components/Styles/Color.jsx
@@ -0,0 +1,10 @@
+export const colors = {
+  Tolopea: '#2E243F',
+  ColumbiaBlue: '#B5FEFE',
+  HotPink: '#FF69B4',
+  Flirt: '#6B2D4F',
+  Razzmatazz: '#CA0873',
+  MidnightBlue: '#123A69',
+  White: '#FFFFFF',
+  LightCyan: '#E1FFFF',
+}
