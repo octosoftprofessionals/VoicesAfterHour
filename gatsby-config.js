b666097524495dbc38de994bfeb63c8508a608f1
Carlos Benetti <benetticarlos1@gmail.com>
2021-10-12 17:26:35 -0300

    absolute path config

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,6 +47,7 @@ module.exports = {
     'gatsby-plugin-sharp',
     'gatsby-plugin-image',
     'gatsby-plugin-react-svg',
+    'gatsby-plugin-root-import',
     {
       resolve: 'gatsby-source-contentful',
       options: contentfulConfig,
