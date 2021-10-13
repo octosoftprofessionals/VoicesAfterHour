f226ecde55acc4325e5ec4a4fd3dea4b89237fac
Roger BC <diolink.4103@gmail.com>
2021-10-13 13:31:09 -0300

    icons are implemented in the navbar and footerbar

--- src/components/Layout/NavBar/index.jsx
+++ src/components/Layout/NavBar/index.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
+
 import { Link } from 'gatsby'
+
 import {
   AppBar,
   Toolbar,
@@ -8,12 +10,14 @@ import {
   IconButton,
   Grid,
 } from '@material-ui/core'
-
 import { Menu } from '@material-ui/icons'
 import { makeStyles } from '@material-ui/core/styles'
+
+import { colors } from '@Styles/Color'
+import LogoVAH from '@Assets/VAH-icon.svg'
+
 import NavLinks from './NavLinks'
 import HideOnScroll from './HideOnScroll'
-import { colors } from '@Styles/Color'
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
@@ -51,25 +55,32 @@ const NavBar = ({ ...props }) => {
       <AppBar position="static" className={classes.appBar}>
         <HideOnScroll {...props}>
           <Toolbar className={classes.toolbar}>
-            <Grid item xs={4}>
-              <Link to={'/'} className={classes.link}>
-                <div className={classes.logo}>{'logo'}</div>
-              </Link>
-            </Grid>
-            <Grid item xs={4}>
-              <Hidden mdDown>
-                <NavLinks className={classes.links} />
-              </Hidden>
-            </Grid>
-            <Grid item xs={4} container justify="flex-end">
-              <Hidden lgUp>
-                <IconButton
-                  aria-label="open drawer"
-                  onClick={() => setShowDrawer(true)}
-                >
-                  <Menu className={classes.iconDrawer} />
-                </IconButton>
-              </Hidden>
+            <Grid
+              container
+              wrap="wrap"
+              justify="space-around"
+              alignItems="center"
+            >
+              <Grid item xs={2}>
+                <Link to={'/'} className={classes.link}>
+                  <LogoVAH />
+                </Link>
+              </Grid>
+              <Grid item xs={4}>
+                <Hidden mdDown>
+                  <NavLinks className={classes.links} />
+                </Hidden>
+              </Grid>
+              <Grid item xs={2} container justify="flex-end">
+                <Hidden lgUp>
+                  <IconButton
+                    aria-label="open drawer"
+                    onClick={() => setShowDrawer(true)}
+                  >
+                    <Menu className={classes.iconDrawer} />
+                  </IconButton>
+                </Hidden>
+              </Grid>
             </Grid>
           </Toolbar>
         </HideOnScroll>
