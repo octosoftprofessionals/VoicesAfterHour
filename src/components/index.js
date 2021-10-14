297ca3c57d0811ee3cd244ce765f56ed7a41bb8f
Agustina Ruidiaz <agusruidiaz@hotmail.com>
2021-10-14 11:21:13 -0300

    relative path config REAL TRUE

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import TitlePage from './General/TitlePage'
+import Banner from './General/Banner'
+import Scroll from './General/Scroll'
+import Home from './Home'
+import PrimaryInput from './General/PrimaryInput'
+
+export { TitlePage, Banner, Scroll, Home, PrimaryInput }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
 import Layout from '@Layout'
-import Home from '@Components/Home'
+import {Home} from '@Components'
 import { useStaticQuery, graphql } from 'gatsby'
+
 const HomePage = (props) => {
   const data = useStaticQuery(graphql`
     query Home {
