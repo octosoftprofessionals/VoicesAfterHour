e47108b41a6c07c883b34c8d6d5e6cbc339ea4e0
Roger BC <diolink.4103@gmail.com>
2021-10-13 21:31:22 -0300

    LandingBanner is created

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 // import { graphql } from 'gatsby'
 
 import Layout from '@Layout'
+import Home from '@Components/Home'
 
 class RootIndex extends React.Component {
   render() {
     return (
       <Layout>
-        <div>HOME</div>
+        <Home />
       </Layout>
     )
   }
