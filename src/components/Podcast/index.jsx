e655b00f68b5dfb7f4b42693c39526472f566c9b
Carlos Benetti <benetticarlos1@gmail.com>
2021-10-18 21:36:19 -0300

    podcast page is conected with contentful

--- src/components/Podcast/index.jsx
+++ src/components/Podcast/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { TitlePage, MainGrid } from '@Components'
 
-const Podcast = () => {
-    const description =
-      'Our team is composed ipsum is simply dummy text of the printing and typesetting industry. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using , content here, making it look like readable English. '
-    return (
-      <MainGrid>
-        <TitlePage title="Podcast" showText text={description} />
-      </MainGrid>
-    )
+const Podcast = ({ data }) => {
+  const {
+    allContentfulPodcastPost: { edges: podcasts },
+  } = data
+  console.log('data :>> ', podcasts)
+  return (
+    <MainGrid>
+      <TitlePage title="Podcast" showText />
+    </MainGrid>
+  )
 }
 
 export default Podcast
--- src/pages/podcast.jsx
+++ src/pages/podcast.jsx
@@ -1,14 +1,37 @@
 import React from 'react'
 import Layout from '@Layout'
 import { Podcast } from '@Components'
+import { useStaticQuery, graphql } from 'gatsby'
 
-const podcast = () => {
-  
+const PodcastPage = () => {
+  const data = useStaticQuery(graphql`
+    query Podcast {
+      allContentfulPodcastPost {
+        edges {
+          node {
+            order
+            title
+            coverImage {
+              file {
+                url
+              }
+            }
+            youtubeUrl
+            spotifyUrl
+            creationDate
+            description {
+              description
+            }
+          }
+        }
+      }
+    }
+  `)
   return (
     <Layout>
-      <Podcast />
+      <Podcast data={data} />
     </Layout>
   )
 }
 
-export default podcast
+export default PodcastPage
