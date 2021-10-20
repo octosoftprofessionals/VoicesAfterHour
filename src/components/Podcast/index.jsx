014416f3afb8ab5da30925d9958268f04b3e9a2e
Carlos Benetti <benetticarlos1@gmail.com>
2021-10-20 18:19:49 -0300

    podcast order by year and month

--- src/components/Podcast/index.jsx
+++ src/components/Podcast/index.jsx
@@ -4,12 +4,12 @@ import PodcastItem from './PodcastItem'
 import { Grid } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import { colors } from '@Styles'
+import { sortBy } from 'lodash'
 
 const Podcast = ({ data }) => {
   const {
     allContentfulPodcastPost: { edges: podcasts },
   } = data
-  console.log('data :>> ', podcasts)
 
   const podcastDumy = [
     {
@@ -31,10 +31,10 @@ const Podcast = ({ data }) => {
       node: { creationDate: '2021-10-04T00:00-03:00' },
     },
     {
-      node: { creationDate: '2021-10-04T00:00-03:00' },
+      node: { creationDate: '2021-10-05T00:00-03:00' },
     },
     {
-      node: { creationDate: '2021-10-04T00:00-03:00' },
+      node: { creationDate: '2021-10-05T00:00-03:00' },
     },
     {
       node: { creationDate: '2021-10-04T00:00-03:00' },
@@ -110,441 +110,123 @@ const Podcast = ({ data }) => {
     },
   ]
 
-  const resDumy = [
-    [
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-    ],
-    [
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-    ],
-    [
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-      [
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-        {
-          node: { creationDate: '2021-10-04T00:00-03:00' },
-        },
-      ],
-    ],
-  ]
-
   const podcastsPerYear = []
 
   const yearsList = []
   podcastDumy.map(({ node }) => {
     let date = new Date(node.creationDate)
     const year = date.getFullYear()
-    console.log('year :>> ', year)
-    // var options = { month: 'long' }
-    // const month = new Intl.DateTimeFormat('en-US', options).format(date)
-    // console.log('mes largo :>>', month)
 
     if (!yearsList.includes(year)) {
       yearsList.push(year)
     }
   })
 
-  for (let i = 0; i < yearsList.length; i++) {
+  const sortYearsList = yearsList.sort()
+
+  for (let i = 0; i < sortYearsList.length; i++) {
     let oneYearPodcasts = podcastDumy.filter((obj) => {
       let date = new Date(obj.node.creationDate)
       const year = date.getFullYear()
 
-      if (year === yearsList[i]) {
+      if (year === sortYearsList[i]) {
         return true
       } else {
         return false
       }
     })
 
-    console.log('oneYearPodcasts :>> ', oneYearPodcasts)
     podcastsPerYear.push(oneYearPodcasts)
+  }
+
+  for (let i = 0; i < podcastsPerYear.length; i++) {
+    const months = {
+      january: [],
+      february: [],
+      march: [],
+      april: [],
+      may: [],
+      june: [],
+      july: [],
+      august: [],
+      september: [],
+      october: [],
+      november: [],
+      december: [],
+    }
+    podcastsPerYear[i].map((obj) => {
+      let date = new Date(obj.node.creationDate)
+      const month = date.getMonth()
+
+      switch (month) {
+        case 0:
+          months.january.push(obj)
+          break
+
+        case 1:
+          months.february.push(obj)
+          break
+
+        case 2:
+          months.march.push(obj)
+          break
+
+        case 3:
+          months.april.push(obj)
+          break
+
+        case 4:
+          months.may.push(obj)
+          break
+
+        case 5:
+          months.june.push(obj)
+          break
+
+        case 6:
+          months.july.push(obj)
+          break
+
+        case 7:
+          months.august.push(obj)
+          break
+
+        case 8:
+          months.september.push(obj)
+          break
+
+        case 9:
+          months.october.push(obj)
+          break
+
+        case 10:
+          months.november.push(obj)
+          break
+
+        case 11:
+          months.december.push(obj)
+          break
+
+        default:
+          break
+      }
+    })
+
+    podcastsPerYear[i] = []
+    podcastsPerYear[i].push(
+      months.january,
+      months.february,
+      months.march,
+      months.april,
+      months.may,
+      months.june,
+      months.july,
+      months.august,
+      months.september,
+      months.october,
+      months.november,
+      months.december
+    )
   }
 
   console.log('podcastsPerYear :>> ', podcastsPerYear)
