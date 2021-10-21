b6767f09dec63a53c73c0dfb19d7f20bb81907f2
Agustina Ruidiaz <agusruidiaz@hotmail.com>
2021-10-21 20:11:48 -0300

    change image size

--- src/components/AboutUs/index.jsx
+++ src/components/AboutUs/index.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
 
 import { TitlePage, MainGrid, Scroll,CardImage } from '@Components'
-import { Grid } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import GridTeamMember from './GridTeamMember'
-import AboutUsBackground from '../../assets/Objects.png'
-import {finedImgUrl} from '@Utils'
 
 const AboutUs = ({ data }) => {
   const {
@@ -26,31 +23,28 @@ const AboutUs = ({ data }) => {
     'Our team is composed ipsum is simply dummy text of the printing and typesetting industry. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using , content here, making it look like readable English. '
 
   return (
-    <MainGrid>
+    <MainGrid className={classes.root}>
       <TitlePage title="About Us" showText text={description} />
       <Scroll title={'Know us'} href={'ourTeam'}>
         <GridTeamMember members={members} />
       </Scroll>
-      <Grid
-        item
-        xs={12}
-        container
-        justify="center"
-        alignItems="flex-end"
-        className={classes.imgContainer}
-      >
-        <CardImage imageUrl={url} />
-      </Grid>
+      <CardImage imageUrl={url} className={classes.imgContainer} />
     </MainGrid>
   )
 }
 
 const useStyles = makeStyles((theme) => ({
-  root: { padding: theme.spacing(20, 0) },
+  root: { postion: 'relative' },
   grid: { rowGap: theme.spacing(14), columnGap: theme.spacing(18) },
   imgContainer: {
-    overflow: 'hidden',
-    height: '40vh',
+    minHeight: '40vh',
+    width: '100%',
+    backgroundSize: 'contain',
+    backgroundPositionX: 'center',
+    backgroundPositionY: '2vh',
+    [theme.breakpoints.down('md')]:{
+      minHeight: '19vh'
+    }
   },
 }))
 
