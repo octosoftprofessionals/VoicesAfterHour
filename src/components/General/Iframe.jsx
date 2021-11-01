8e3f1c98ff09a3afaed4605747ca80b5f8f01317
Carlos Benetti <benetticarlos1@gmail.com>
2021-11-01 19:31:01 -0300

    close button spotify item

--- src/components/General/Iframe.jsx
+++ src/components/General/Iframe.jsx
@@ -1,19 +1,7 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-
-const useStyles = makeStyles((theme) => ({
-  cardMediaS: {
-    width: 300,
-    height: 80,
-    border: 'none',
-    overflow: 'hidden',
-    borderRadius: 20,
-  },
-  cardMediaL: {
-    height: 380,
-    borderRadius: 20,
-  },
-}))
+import CancelOutlinedIcon from '@material-ui/icons/CancelOutlined'
+import { IconButton } from '@material-ui/core'
 
 const Iframe = ({ spotifyLink, ...props }) => {
   const classes = useStyles()
@@ -23,14 +11,48 @@ const Iframe = ({ spotifyLink, ...props }) => {
 
   const trackRef = `${match[1]}/${match[2]}`
   return (
-    <iframe
-      src={`https://open.spotify.com/embed/${trackRef}`}
-      className={
-        match[1] === 'playlist' ? classes.cardMediaL : classes.cardMediaS
-      }
-      {...props}
-    ></iframe>
+    <div className={classes.root}>
+      <iframe
+        src={`https://open.spotify.com/embed/${trackRef}`}
+        className={
+          match[1] === 'playlist' ? classes.cardMediaL : classes.cardMediaS
+        }
+        {...props}
+      />
+      <IconButton className={classes.closeButton}>
+        <CancelOutlinedIcon />
+      </IconButton>
+    </div>
   )
 }
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignContent: 'flex-end',
+    alignItems: 'flex-start',
+    flex: 1,
+  },
+  cardMediaS: {
+    width: '100%',
+    height: 80,
+    border: 'none',
+    overflow: 'hidden',
+    borderRadius: 6,
+  },
+  cardMediaL: {
+    height: 380,
+    borderRadius: 6,
+  },
+  closeButton: {
+    background: 'none',
+    borderStyle: 'none',
+    color: 'white',
+    width: 24,
+    height: 24,
+    display: 'contents',
+  },
+}))
 
 export default Iframe
